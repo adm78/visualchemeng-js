4de94097bcf27cc0c9659d5afea251d88d6857a9
Andrew D. McGuire <amcguire227@gmail.com>
2017-10-18 00:42:41 +0100

    Additions to support reflection collision in md.js

--- md.js
+++ md.js
@@ -12,7 +12,8 @@ function setup() {
     xmax = min(772+28,windowWidth-2*28);
     ymax = xmax*0.618;
     var canvas= createCanvas(xmax, ymax);
-    part_to_init = Math.round(xmax*ymax/6000.0);
+    //part_to_init = Math.round(xmax*ymax/6000.0);
+    part_to_init = 66;
     console.log("xmax, ymax = ", xmax, ymax);
     console.log("part_to_init = ", part_to_init);
     particles = initParticles(part_to_init,r,xmax,ymax);
@@ -80,39 +81,79 @@ function doStep(dt) {
 	
 }
 
-function getCollisionTime(Part1, Part2) {
-    //compute the time to pass for a collsion to take place
-    //currenrtly does not respect periodic boundary conditions!!!
-    var deltaVel = p5.Vector.sub(Part1.vel,Part2.vel)
-    var deltaPos = p5.Vector.sub(Part1.pos,Part2.pos)
-    var minDist = Part1.radius + Part2.radius
-    var a = p5.Vector.dot(deltaVel, deltaVel)
-    var b = 2.0*p5.Vector.dot(deltaPos,deltaVel)
-    var c = p5.Vector.dot(deltaPos,deltaPos) - minDist*minDist
-    var discrim = b*b - 4*a*c
+function getCollisonTime(v1,v2,p1,p2,r1,r2) {
 
-    if ((discrim > 0) && (b < 0)) {
-	var t1 = (-b - Math.sqrt(discrim))/(2*a)
+    // get the collision time for a collision pair the following
+    // properties
+    var deltaVel = p5.Vector.sub(v1,v2);
+    var deltaPos = p5.Vector.sub(p1,p2);
+    var minDist = r1 + r2;
+    var a = p5.Vector.dot(deltaVel, deltaVel);
+    var b = 2.0*p5.Vector.dot(deltaPos,deltaVel);
+    var c = p5.Vector.dot(deltaPos,deltaPos) - minDist*minDist;
+    var discrim = b*b - 4*a*c;
+
+    if (discrim > 0 && b < 0) {
+	var t1 = (-b - Math.sqrt(discrim))/(2*a);
 	return t1
     }
-    return NaN 
+    console.log("getCollisonTime: discrim = ", discrim);
+    return NaN     
+}
+
+function getCollisionTimeParticles(Part1, Part2) {
+
+    // checks the collision time between Part1
+    // and each reflection of Part2 and returns
+    // the time of the first collision.
+    var tmin;
+    var tcurr;
+    
+    // generate the ficticous coordinates of each reflection
+    var preflects = [
+    	createVector(Part2.pos.x, Part2.pos.y - ymax),
+    	createVector(Part2.pos.x, Part2.pos.y + ymax),
+    	createVector(Part2.pos.x - xmax, Part2.pos.y),
+    	createVector(Part2.pos.x + xmax, Part2.pos.y)
+    ];
+    console.log("Considering collision between:", Part1, Part2);
+    tmin = getCollisonTime(Part1.vel,Part2.vel,Part1.pos,Part2.pos,Part1.radius,Part2.radius);
+    console.log("tmin no reflect = ", tmin);
+    var i;
+    for (i = 0; i < preflects.length; i++) {
+	console.log("testing for collision with i = ", i , ", coordinates =");
+	console.log(Part1.vel,Part2.vel,Part1.pos,preflects[i],Part1.radius,Part2.radius);
+    	tcurr = getCollisonTime(Part1.vel,Part2.vel,Part1.pos,preflects[i],Part1.radius,Part2.radius);
+	console.log("tcurr = ", tcurr);
+    	if (tcurr < tmin) {
+    	    tmin = tcurr;
+    	}
+    }
+    console.log("tmin = ", tmin);
+    return tmin
+    
 }
 
 function getCollisionList(particles) {
 
     // compute a sorted list of collision times
     var coll_list = []
+    var i, j;
     for (i = 0; i < particles.length; i++) {
-	for (j = i+1; j < particles.length; j++)
+	for (j = i+1; j < particles.length; j++) {
 	    if (i != j) {
-		var col_time = getCollisionTime(particles[i],particles[j])
+		console.log("getCollisionList: i = ",i, " j = ",j);
+		var col_time = getCollisionTimeParticles(particles[i],particles[j]);
 		if (isNaN(col_time) != true) {
-		    var coll = [col_time, i, j]
-		    coll_list.push(coll)
+		    var coll = [col_time, i, j];
+		    coll_list.push(coll);
 		}
-	    }		
-    }
-    coll_list.sort(function(a,b){return a[0] - b[0]})
+	    } else {
+		console.log("getCollisionList: skipping i = ",i, " j = ",j);	
+	    }
+	};
+    };
+    coll_list.sort(function(a,b){return a[0] - b[0]});
     return coll_list
 }
 
@@ -143,7 +184,7 @@ function initParticles(n,r,xmax,ymax) {
     	// 			    random(ymax),
     	// 			    xmax,ymax,r);
 	for (j = 0; j < Math.round(ymax/dx); j++) {
-	    if (n_init < n-1) {
+	    if (n_init < n) {
     		parts[n_init] = new Particle(dx*(i+0.5),dx*(j+0.5),xmax,ymax,r);
 		console.log("intialised particle",n_init," with x, y = ", dx*(i+1),dx*(j+1));
 		parts[n_init].show();
