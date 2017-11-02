992ed95ca50c724987f54d6a7cb768819a621342
Andrew D. McGuire <amcguire227@gmail.com>
2017-11-02 22:38:42 +0000

    adding some plotly demos that can be adpated for use in the kmc module

--- modules/kmc/kmc.js
+++ modules/kmc/kmc.js
@@ -20,10 +20,53 @@
 // Set-up parameters
 
 
+// try some plotting
+console.log("Hello world. I'm going to be the VCE Kinetic Monte Carlo Module.")
+console.log("But, for now, let's see if we can show a some graphs!")
+
+TESTER = document.getElementById('tester');
+Plotly.plot( TESTER, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], {
+	margin: { t: 0 } } );
+
+
+Plotly.d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'AAPL High',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'AAPL.High'),
+  line: {color: '#17BECF'}
+}
+
+var trace2 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'AAPL Low',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'AAPL.Low'),
+  line: {color: '#7F7F7F'}
+}
+
+var data = [trace1,trace2];
+
+var layout = {
+  title: 'Basic Time Series',
+};
+
+Plotly.newPlot('myDiv', data, layout);
+})
+
 // --------------------------------------------------
 //             kmc functionality
 //---------------------------------------------------
 
-// Let's try and plot a simple sinusoid to the canvas as 
-// a test
-console.log("Hello world. I'm going to be the VCE Kinetic Monte Carlo Module")
+
