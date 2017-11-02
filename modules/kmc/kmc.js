f3ff76015e3ade035f2808a846a3af7a3554b86a
Andrew D. McGuire <amcguire227@gmail.com>
2017-11-02 22:24:30 +0000

    adding the templates for the kmc main script and test page

---
+++ modules/kmc/kmc.js
@@ -0,0 +1,29 @@
+// VCE Project - kmc.js Kinetic Monte Carlo (KMC)
+//
+// This script performs a simple, kinetic Monte Carlo simulation of
+// the reaction: A <==> B <==> C
+//
+// Requires:
+// - p5.js or p5.min.js
+// - grafica-0.1.0.min.js or grafica.min.js or grafica.js
+//
+// Andrew D. McGuire, Gustavo Leon 2017
+// (a.mcguire227@gmail.com)
+//----------------------------------------------------------
+
+
+
+// --------------------------------------------------
+//             visualisation functionality
+// ------------------------------------------------
+//
+// Set-up parameters
+
+
+// --------------------------------------------------
+//             kmc functionality
+//---------------------------------------------------
+
+// Let's try and plot a simple sinusoid to the canvas as 
+// a test
+console.log("Hello world. I'm going to be the VCE Kinetic Monte Carlo Module")
