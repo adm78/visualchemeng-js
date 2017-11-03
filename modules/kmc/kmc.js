6ae6bd67f89ac32dfc10d715d2b9aa6593282e12
Andrew McGuire <amcguire227@gmail.com>
2017-11-03 21:42:29 +0000

    adding basic time-stepping functionality to the kmc module

--- modules/kmc/kmc.js
+++ modules/kmc/kmc.js
@@ -33,7 +33,7 @@ Plotly.plot( TESTER, [{
 
 Plotly.d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
 
-  function unpack(rows, key) {
+    function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
 }
 
@@ -68,5 +68,81 @@ Plotly.newPlot('myDiv', data, layout);
 // --------------------------------------------------
 //             kmc functionality
 //---------------------------------------------------
+// initial conditions
+var NA = 10000;
+var NB = 0;
+var NC = 0;
+var Ntot_init = NA + NB + NC;
+var time = 0.0
+var tmax = 1000.0;
+var steps = 0;
+var smax = 100000;
+
+// composition vectors
+var NAvec = [];
+var NBvec = [];
+var NCvec = [];
+var NA_ss = []
+var NB_ss = [];
+var NC_ss = [];
+
+var time_vec = [time];
+
+// rate constants
+var k1 = 10.0; // A --> B, r1 = k1*NA
+var kb1 = 5.0; // B --> A, rb1 = kb1*NB
+var k2 = 10.0; // B --> C, r2 = k2*NB
+var kb2 = 5.0; // C --> B, rb2 = kb2*NC
+
+while (time < tmax && steps < smax) {
+
+    var rates = getRates();
+    var cum_rates = getCumulativeRates(rates);
+    var total_rate = cum_rates[cum_rates.length-1];
+    var q = Math.random()*total_rate;
+    var dt = - Math.log(q)/total_rate
+    doJump(q,cum_rates);
+    time = time + dt;
+    steps += 1;
+    
+}
+console.log("Time evolution completed!")
+
+function getRates() {
+    // compute the rates
+    var r1 = k1*NA;
+    var rb1 = kb1*NB;
+    var r2 = k2*NB;
+    var rb2 = kb2*NC
+    return [r1, rb1, r2, rb2];
+}
 
 
+function getCumulativeRates(rl) {
+    // return the cumulative rates list
+    for (i = 1; i < rl.length; i++) {
+	rl[i] += rl[i-1];
+    }
+    return rl
+}
+
+function doJump(q,cr) {
+    // perform one of the jump processes
+    if (q <= cr[0]) {
+	NA -= 1;
+	NB += 1;
+    }
+    else if (q < cr[1]) {
+	NB -= 1;
+	NA += 1;
+    }
+    else if (q < cr[2]) {
+	NB -= 1;
+	NC += 1;
+    }
+    else if (q < cr[3]) {
+	NC -= 1;
+	NB += 1;
+    }
+    
+}
