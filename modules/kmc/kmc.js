b38ae8b37a285fbe0c6d03bdeed414743741873a
Andrew D. McGuire <amcguire227@gmail.com>
2017-11-08 21:08:44 +0000

    Major refactoring of the kmc module, adding storage/solution classes. Concentration plot now functional

--- modules/kmc/kmc.js
+++ modules/kmc/kmc.js
@@ -1,69 +1,19 @@
 // VCE Project - kmc.js Kinetic Monte Carlo (KMC)
 //
-// This script performs a simple, kinetic Monte Carlo simulation of
-// the reaction: A <==> B <==> C
+// This script performs a simple, kinetic Monte Carlo
+// simulation of the reaction:
+//
+//                   A <==> B <==> C
 //
 // Requires:
-// - p5.js or p5.min.js
-// - grafica-0.1.0.min.js or grafica.min.js or grafica.js
+// - plotly-latest.min.js
+// - kmc_storage.js
+// - kmc_solution.js
 //
 // Andrew D. McGuire, Gustavo Leon 2017
 // (a.mcguire227@gmail.com)
 //----------------------------------------------------------
 
-
-
-// --------------------------------------------------
-//             visualisation functionality
-// ------------------------------------------------
-//
-// Set-up parameters
-
-
-// try some plotting
-console.log("Hello world. I'm going to be the VCE Kinetic Monte Carlo Module.")
-console.log("But, for now, let's see if we can show a some graphs!")
-
-TESTER = document.getElementById('tester');
-Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
-
-
-Plotly.d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
-
-    function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
-}
-
-
-var trace1 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'AAPL High',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.High'),
-  line: {color: '#17BECF'}
-}
-
-var trace2 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'AAPL Low',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.Low'),
-  line: {color: '#7F7F7F'}
-}
-
-var data = [trace1,trace2];
-
-var layout = {
-  title: 'Basic Time Series',
-};
-
-Plotly.newPlot('myDiv', data, layout);
-})
 
 // --------------------------------------------------
 //             kmc functionality
@@ -72,77 +22,143 @@ Plotly.newPlot('myDiv', data, layout);
 var NA = 10000;
 var NB = 0;
 var NC = 0;
-var Ntot_init = NA + NB + NC;
-var time = 0.0
+var time = 0.0;
+
+// final conditions
 var tmax = 1000.0;
-var steps = 0;
 var smax = 100000;
 
-// composition vectors
-var NAvec = [];
-var NBvec = [];
-var NCvec = [];
-var NA_ss = []
-var NB_ss = [];
-var NC_ss = [];
-
-var time_vec = [time];
+// storage/solution intialisation
+var kmc_Solution = new Solution(NA,NB,NC,time);
+var kmc_Storage = new Storage(kmc_Solution);
+var ss_Solution = new Solution(NA,NB,NC,time)
 
 // rate constants
-var k1 = 10.0; // A --> B, r1 = k1*NA
-var kb1 = 5.0; // B --> A, rb1 = kb1*NB
-var k2 = 10.0; // B --> C, r2 = k2*NB
-var kb2 = 5.0; // C --> B, rb2 = kb2*NC
+// A --> B, r1 = k1*NA
+// B --> A, rb1 = kb1*NB
+// B --> C, r2 = k2*NB
+// C --> B, rb2 = kb2*NC
+var rate_consts = {k1:10.0,kb1:5.0,k2:10.0,kb2:5.0};
 
-while (time < tmax && steps < smax) {
 
-    var rates = getRates();
-    var cum_rates = getCumulativeRates(rates);
-    var total_rate = cum_rates[cum_rates.length-1];
-    var q = Math.random()*total_rate;
-    var dt = - Math.log(q)/total_rate
-    doJump(q,cum_rates);
-    time = time + dt;
-    steps += 1;
+function jumpLoop(solution,rate_consts,tmax,smax,storage) {
+
+    // Simulate using kmc to tmax with intial conditions
+    // given by 'solution'. Not exceeding smax steps.
+    // storage is a validate Storage object to collect data. 
     
+    var steps = 0;
+    while (solution.time < tmax && steps < smax) {
+	
+	var rates = getRates(solution,rate_consts);
+	var cum_rates = getCumulativeRates(rates);
+	var total_rate = cum_rates[cum_rates.length-1];
+	var q = Math.random()*total_rate;
+	var dt = - Math.log(Math.random())/total_rate;
+	var solution = doJump(solution,q,cum_rates,dt); // update the solution
+	storage.update(solution); 
+	steps += 1;
+	
+    }
+    return {solution: solution,
+	    storage: storage};
 }
-console.log("Time evolution completed!")
 
-function getRates() {
-    // compute the rates
-    var r1 = k1*NA;
-    var rb1 = kb1*NB;
-    var r2 = k2*NB;
-    var rb2 = kb2*NC
+
+function getRates(s,rc) {
+
+    // Compute the rates given solution state s
+    // and rate contants array rc.
+    
+    var r1 = rc.k1*s.NA;
+    var rb1 = rc.kb1*s.NB;
+    var r2 = rc.k2*s.NB;
+    var rb2 = rc.kb2*s.NC;
     return [r1, rb1, r2, rb2];
 }
 
 
 function getCumulativeRates(rl) {
-    // return the cumulative rates list
+
+    // Return the cumulative rates list
+    
     for (i = 1; i < rl.length; i++) {
 	rl[i] += rl[i-1];
     }
     return rl
 }
 
-function doJump(q,cr) {
-    // perform one of the jump processes
+function doJump(s,q,cr,dt) {
+    
+    // Perform one of the jump processes on solution
+    // s according random deviate q, cumualtive
+    // rate array cr and time step dt.
+    // Returns new solution.
+    
     if (q <= cr[0]) {
-	NA -= 1;
-	NB += 1;
+	s.NA -= 1;
+	s.NB += 1;
     }
     else if (q < cr[1]) {
-	NB -= 1;
-	NA += 1;
+	s.NB -= 1;
+	s.NA += 1;
     }
     else if (q < cr[2]) {
-	NB -= 1;
-	NC += 1;
+	s.NB -= 1;
+	s.NC += 1;
     }
     else if (q < cr[3]) {
-	NC -= 1;
-	NB += 1;
+	s.NC -= 1;
+	s.NB += 1;
     }
+
+    // update the time
+    s.time = s.time + dt    
     
+    return s
 }
+
+result = jumpLoop(kmc_Solution,rate_consts,tmax,smax,kmc_Storage)
+kmc_Solution = result.solution;
+kmc_Storage = result.storage;
+console.log("Time evolution completed!")
+
+// --------------------------------------------------
+//             visualisation functionality
+// --------------------------------------------------
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'NA kmc',
+  x: kmc_Storage.time,
+  y: kmc_Storage.NA,
+  line: {color: '#17BECF'}
+}
+
+var trace2 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'NB kmc',
+  x: kmc_Storage.time,
+  y: kmc_Storage.NB,
+  line: {color: '#7F7F7F'}
+}
+
+var trace3 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'NC kmc',
+  x: kmc_Storage.time,
+  y: kmc_Storage.NC,
+  line: {color: '#EE9A00'}
+}
+
+
+var data = [trace1,trace2,trace3];
+
+var layout = {
+  title: 'Concentration evolution',
+};
+
+Plotly.newPlot('myDiv', data, layout);
+    
---
+++ modules/kmc/kmc_storage.js
@@ -0,0 +1,44 @@
+// VCE Project - kmc_storage.js Kinetic Monte Carlo (KMC)
+//
+// This file contains the data storage class used within the
+// kmc modules. 
+//
+// Requires:
+// - kmc_solution.js
+//
+// Andrew D. McGuire, Gustavo Leon 2017
+// (a.mcguire227@gmail.com)
+//----------------------------------------------------------
+
+function Storage(solution) {
+
+    // concentration histories
+    this.NA  = [solution.NA];
+    this.NB = [solution.NA];
+    this.NC = [solution.NA];
+    this.Ntot = [solution.NA+solution.NB+solution.NC];
+    this.time = [solution.time];
+
+    this.update = function(solution) {
+
+	// updates the concentration histoies
+	// using the new data point.
+	this.NA.push(solution.NA);
+	this.NB.push(solution.NB);
+	this.NC.push(solution.NC);
+	this.Ntot.push(solution.NA+solution.NB+solution.NC);
+	this.time.push(solution.time);
+
+    }
+
+    function clear() {
+
+	// purge the container
+	this.NA = []; 
+	this.NB = [];
+	this.NC = [];
+	this.time = [];	
+    }
+
+}
+
