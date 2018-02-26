3eff0a6c2d74e139b2f065c1c6b5137f99172ef2
Fraser M Baigent (Office) <fraser.baigent@strath.ac.uk>
2018-02-26 18:22:23 +0000

    code works now, ugly on the graph but you can see as it heats up. make sure to keep dt low!

--- modules/heat/heatrod.py
+++ modules/heat/heatrod.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 ############################################################
 ############################################################
@@ -25,11 +25,11 @@ alpha = 0.01                      # rod diffusivity
 
  # SIMULATION PARAMETERS
 
-n = 10.0                          # number of nodes
+n = 50                            # number of nodes
 delta_x = l/n                     # finite difference x
 t_0 = 0.0                         # initial time
-t_end = 1.0                       # end time
-n_steps = 100.0                   # number of time steps
+t_end = .5                       # end time
+n_steps = 20.0                   # number of time steps
 delta_t = t_end/ n_steps          # time step
 q = alpha * delta_t / delta_x **2 # forward step parameter, must be < 0.5 for stable solutions
 
@@ -39,19 +39,12 @@ q = alpha * delta_t / delta_x **2 # forward step parameter, must be < 0.5 for st
 
  # INITIAL CONDITIONS
 
-dirichlet = 10.0                 # dirichlet boundary condition
-dirichlet_end = 2.0
-f0 = 1.0                          # initial temperature constant
-t = t_0 + delta_t                 # set loop first time step
-phi_0 = np.ones(n) * f0           # initial temperature vector
+dirichlet = 10.0                  # dirichlet boundary condition x=0
+dirichlet_end = 0.50              # End boundary condition x=L
+f0 = 1.0    
+phi_0 = np.full(n, f0)            # initial temperature vector
 phi_0[0] = dirichlet
 phi_0[-1] = dirichlet_end
-############################################################
-############################################################
-############################################################
-
- # BUILD RESULTS STORAGE
-results = {}                      # results matrix dict
 
 ############################################################
 ############################################################
@@ -59,30 +52,44 @@ results = {}                      # results matrix dict
 
  # ALGORITHM
 
-def main(results, phi, t, q):
-    results[t] = phi
+def main(phi, t, q, delta_t):    
+    
     # Execute main code, evaluating temperature across bar
     # from intial time until end time and returning results
     # as a dictionary of matrices
     
-    while t <= t_end:  
-        t+= delta_t
+    x = np.arange(0,l,delta_x)
+    while t <= t_end:
+        plotter(phi,x,t)
+        t += delta_t
         phi_old = phi
 
         # Loop through and update the matrix with the new values, noting
         # the boundaries are fixed in this case (first/last never get
         # updated)
          
-        for i in range(1,len(phi_old)): # very non-pythonic...
+        for i in range(1,len(phi_old)-1): # very non-pythonic...
             phi[i] = q*(phi_old[i+1] + phi_old[i-1]) + (1-2*q)*phi_old[i]
-            
-        results[t] = phi
+        
 
-    return results
 
 ############################################################
 ############################################################
 ############################################################
 
-results = main(results,phi,t,q)
-print results
+ # PLOT GRAPH FOR 5 TIME STEPS
+
+def plotter(phi,x,t):
+    plt.plot(x,phi,label = r'$t = %2.02f$' % float(t))
+
+
+############################################################
+############################################################
+############################################################
+
+ # PLOT GRAPH FOR n_steps TIME STEPS
+
+main(phi_0,t_0, q, delta_t)
+plt.legend()
+
+plt.show()
