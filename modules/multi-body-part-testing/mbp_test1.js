c95f8dcb2d714a09b73944c05905f977f7e09b44
Andrew D. McGuire <amcguire227@gmail.com>
2018-05-22 22:39:48 +0100

    lib: adding support for multi-body particles.
    New MultiBodyParticle class has been added to store and manipulate
    multiple constrained particles (such as those in diatomic particle,
    polymer etc.). See vce_particle.MultiBodyParticles for details. Still
    some missing functionality here. A test module
    /modules/multi-body-part-testing/mbp_test1.js has been added (run with
    /tests/multi_body_part_test.html).

---
+++ modules/multi-body-part-testing/mbp_test1.js
@@ -0,0 +1,157 @@
+// VCE Project - mbp_test1.js
+//
+// Testing platform for the vce_particle.MultiBodyParticle class.
+//
+// Requires:
+// - p5.js or p5.min.js
+// - matter.js
+// - vce_utils.js
+// - vce_particle.js
+// - vce_ensemble.js
+//
+// Andrew D. McGuire 2018
+// a.mcguire227@gmail.com
+//
+// --------------------------------------------------
+//               set-up variables
+// --------------------------------------------------
+var debug = false;
+var paused_log = false;
+var ensemble;
+var boundaries = [];
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint;
+var sid;
+var show_boundaries = false;
+
+// --------------------------------------------------
+//             Visualisation functionality
+// --------------------------------------------------
+function preload() {
+    var tank_URL = "../resources/reactor_ni.svg";
+    tank = loadImage(tank_URL, pic => print(pic), loadImgErrFix);
+};
+
+function setup() {
+
+    /* This function is called upon entry to create the
+       simulation canvas which we draw onto  */
+
+    // set-up the canvas
+    //var dimensions = getSimBoxDimensions();
+    xmax = 400;//dimensions.xmax;
+    ymax = 400;//dimensions.ymax;
+    var canvas= createCanvas(xmax, ymax);
+    canvas.parent("sim_container");
+    sid = getImgScaledDimensions(tank, 0.6, ymax);
+    
+    // set-up the physics engine
+    engine = Engine.create();
+    world = engine.world;
+
+    // initialise the ensemble
+    ensemble = new Ensemble([],world);
+    
+    boundaries.push(new Boundary((xmax-sid.width)/2,
+				 (ymax)/2,
+				 20, sid.height*0.7, 0.0, world));
+    boundaries.push(new Boundary((xmax+sid.width)/2,
+				 (ymax)/2,
+				 20, sid.height*0.7, 0.0, world));
+    boundaries.push(new Boundary((xmax)/2,
+				 (ymax+1.0*sid.height)/2,
+				 sid.width, 20, 0.0, world));
+    boundaries.push(new Boundary((xmax-sid.width*0.7)/2,
+				 (ymax+0.9*sid.height)/2,
+				 0.5*sid.width, 20, 0.7, world));
+    boundaries.push(new Boundary((xmax+sid.width*0.7)/2,
+				 (ymax+0.9*sid.height)/2,
+				 0.5*sid.width, 20, 2*PI-0.7, world));
+
+    
+};
+
+function draw() {
+
+    /* Draws background and img to the canvas.
+       This function is continuously called for the
+       lifetime of the scripts executions after setup()
+       has completed. Effectively advances time. */
+    
+    background(51);
+    imageMode(CENTER);
+    image(tank, xmax/2 , ymax/2, sid.width, sid.height);
+    if (!paused_log) {
+	Engine.update(engine);
+	ensemble.removeOutliers(xmax,ymax);
+    };
+    ensemble.show();
+    if (show_boundaries) {
+	    for (var i = 0; i < boundaries.length; i++) {
+		boundaries[i].show();
+	    };
+    };
+    //console.log(ensemble.particles.length, world.bodies.length);
+};
+
+function DiatomicParticle(world, x, y, bl, theta, colours) {
+    // First attempt at a diamtomic particle. A wrapper around
+    // vce-particle.MultiBodyParticle.
+    //
+    // Args:
+    
+    // get particle positions
+    dx = 0.5*bl*Math.cos(theta)
+    dy = 0.5*bl*Math.sin(theta)
+    var p1 = new PhysEngineParticle(world, x+dx, y+dy, 5.0, colours[0]);
+    var p2 = new PhysEngineParticle(world, x-dx, y-dy, 10.0, colours[1]);
+    var bond = Constraint.create({
+        // pointA: { x: 280, y: 120 },
+	bodyA: p1.body,
+        bodyB: p2.body,
+        // pointB: { x: -10, y: -7 },
+        stiffness: 1.0
+    });
+    World.add(world, [bond]);
+    var diatomic = new MultiBodyParticle([p1,p2],[bond])
+    return diatomic
+};
+
+
+//================================================
+// Interactivity
+//================================================
+function mousePressed() {
+    ensemble.addParticle(new DiatomicParticle(world, mouseX, mouseY, 20.0, 0.0, ['#008CBA','#BC0CDF']));
+    console.log(ensemble);
+};
+
+// run button
+$('#run').click(async function(){
+
+    // run/pause button functionality
+    console.log("You just clicked stream/pause!");
+    paused_log = !(paused_log);
+    if (paused_log) {
+	$("#run").text('Run');
+    }
+    else {
+	$("#run").text('Pause');
+    }
+});
+
+// show boundaries button
+$('#restart').click(async function(){
+
+    // boundary show hide
+    console.log("You just clicked show boundaries!");
+    show_boundaries = !(show_boundaries);
+    if (!show_boundaries) {
+	$("#restart").text('Show Bounds');
+    }
+    else {
+	$("#restart").text('Hide Bounds');
+    }
+});
