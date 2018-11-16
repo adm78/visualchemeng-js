1cef5f14abbe4f83950687538a43f986836897fa
Andrew D. McGuire <andrew.mcguire@spirocontrol.com>
2018-11-15 21:57:05 -0500

    lib + heat_exchanger: adding/using simpsons numerical integrator routine, fixing Cp_liq method

--- apps/heat_exchanger/scripts/heat_exchanger_backend.js
+++ apps/heat_exchanger/scripts/heat_exchanger_backend.js
@@ -25,14 +25,13 @@ function HeatExchanger() {
     
     // Methods
     this.duty = function() {
+	// Exchanger duty (W)
 	return this.F*this.delta_h();
     };
     
     this.delta_h = function() {
 	// Enthalpy change (J)
-	
-	// we need a proper numerical integrator for this part
-	return this.component.Cp_liq(0.5*(this.T_out + this.T_in))*(this.T_out - this.T_in);
+	return vce_math.integrator.simpson(this.component.Cp_liq, this.T_in, this.T_out, 50)
     };
 
 };
@@ -47,8 +46,12 @@ function Methanol() {
     this.E_liq = 0.00003;
  
     
-    this.Cp_liq = function(T) {
+    this.Cp_liq = (function(T) {
 	// Cp_liq (J/mol.K)
 	return this.A_liq + this.B_liq*T + this.C_liq*Math.pow(T,2) + this.D_liq*Math.pow(T,3) + this.E_liq*Math.pow(T,4);
-    };
+    }).bind(this)
+    // @Note: 'bind' allows this method to be used by callers other than
+    // Methanol, whilst still pointing `this` at Methanol (instead of the
+    // caller object instance). See
+    // https://stackoverflow.com/a/20279485/4530680 for more info.
 };
